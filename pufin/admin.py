91fb234d1fd9147aa5ee5bdf43260b14a370d3d8
abhi <messi10ap@VM14.04>
2017-03-20 20:56:35 -0400

    asdfasdf

--- pufin/admin.py
+++ pufin/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import UserInfo
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    fields = ['first_name', 'last_name']
+    list_display = ['first_name','last_name']
+
+    def first_name(self,obj):
+        return obj.user.first_name
+
+    def last_name(self,obj):
+        return obj.user.last_name
+
+admin.site.register(UserInfo, UserAdmin)
